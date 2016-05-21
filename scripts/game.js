94debbe67053d0191e5e8e708ffd42d74123fc49
MegaJ <ryasurethatschocolateicecream@gmail.com>
2016-05-21 14:17:05 -0500

    adding math.js library is actually hard. Window.math = math if math is the name of the parameter did not work. Ignore other changes, it's a mess right now.

--- scripts/game.js
+++ scripts/game.js
@@ -1,12 +1,18 @@
-Window.onload = require(["fabric.min", "model"],
-			function(dep1, dep2) {
-			    Window.fabric = dep1;
-			    Window.model = dep2;
+Window.onload = require(["fabric.min", "math", "model"],
+			function(fabric, mathjs, model) {
+			    Window.fabric = fabric;
+			    Window.math = math = mathjs;
+			    Window.model = model;
 			    game();
 			});
 
 function game() { 
     initialize();
+    // while (true) {
+	processInput();
+	update();
+	render();
+    //}
 }
 
 function initialize() {
@@ -32,7 +38,7 @@ function initialize() {
     for (var i = 0; i < 5; i++) {
     	canvas.add(new fabric.Rect({
     	    left: 20*i + 100,
-    	    top: 20*i + 100,
+    	    top: 100,
     	    fill: 'red',
     	    width: 20,
     	    get height() {
@@ -50,7 +56,58 @@ function initialize() {
 	radius: 20, fill: 'green', left: 100, top: 100
     });
     canvas.add(circle);
+
+    //renderPolygon();
+    var paddleLength = 30;
+    var paddleWidth = 20;
+    createPaddle(canvasWidth/2, canvasHeight/2,
+		 paddleLength, paddleWidth, 45);
     canvas.renderAll();
+}
+
+function createPaddle(x, y, length, width, angle) {
+    //angle = angle * (Math.PI/180); // convert to radians
+    
+    var theta = 90 - angle;
+    var pivot = {x : x, y : y};
+
+    // canvas coordinate system is 0,0 for top left corner
+    var right_side_top = {x: x + length * math.cos(math.unit(theta, 'deg')),
+			  y: y - length * math.sin(math.unit(theta, 'deg'))}
+    console.log(right_side_top)
+    line = new fabric.Line([pivot.x, pivot.y,
+			    right_side_top.x, right_side_top.y], {
+	fill: 'green',
+	stroke: 'green'
+    });
+    canvas.add(line);
+    Window.line = line;
+
+    var offsetPoint = {x: x, y: pivot.y - width};
+    
+    line2 =  new fabric.Line([offsetPoint.x, offsetPoint.y,
+			    right_side_top.x, right_side_top.y], {
+	fill: 'green',
+	stroke: 'green'
+    }); 
+    
+    paddle =  new fabric.Polygon([
+	{ x: 10, y: 10 },
+	{ x: 50, y: 30 },
+	{ x: 40, y: 70 },
+	{ x: 60, y: 50 },
+	{ x: 100, y: 150 },
+	{ x: 40, y: 100 }
+    ], {
+	stroke: 'blue',
+	fill: 'rgba(0,0,255,0.75)',
+	strokeWidth: 3
+	
+    }).setAngle(30);
+    Window.paddle = paddle;
+
+    //console.log(paddle.)
+    canvas.add(paddle);
 }
 
 function processInput() {
@@ -64,5 +121,3 @@ function update() {
 function render() {
 
 }
-
-//window.onload = game;
